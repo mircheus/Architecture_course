0c1364f2610f31360bb47aa8e000ed3c4e8a18b5
Miroslav Chernyavskiy <mircheuss@yandex.ru>
2024-05-11 22:41:17 +0200

    Finish HW_1_4

---
+++ Assets/Homework_1/HW_1_4/Scripts/BalloonContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Homework_1.HW_1_4
+{
+    public class BalloonContainer : MonoBehaviour
+    {
+        private IPointsCounter _pointsCounter;
+
+        [SerializeField] private List<Balloon> _ballons = new List<Balloon>();
+        
+        public IPointsCounter PointsCounter => _pointsCounter;
+        public List<Balloon> Ballons => _ballons;
+        public int BalloonsCount => _ballons.Count;
+
+        private void OnEnable()
+        {
+            foreach (var ballon in _ballons)
+            {
+                ballon.Interacted += OnBalloonInteracted;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var ballon in _ballons)
+            {
+                ballon.Interacted -= OnBalloonInteracted;
+            }
+        }
+        
+        public void SetPointsCounter(IPointsCounter pointsCounter)
+        {
+            _pointsCounter = pointsCounter;
+        }
+
+        private void OnBalloonInteracted(Balloon balloon)
+        {
+            _pointsCounter.AddPoint(balloon);
+        }
+    }
+}
